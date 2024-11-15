243ae1e09c76f0e848854e12b3d5e62d808046e8
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-11-15 15:49:32 +1300

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { useEffect, useState } from 'react'
 export default function Home() {
   const [width, setWidth] = useState(0)
   const [height, setHeight] = useState(0)
+  const [ratio, setRatio] = useState(0)
 
   useEffect(() => {
     if (typeof window === 'undefined') return
     const handleResize = () => {
       setWidth(window.innerWidth)
       setHeight(window.innerHeight)
+      setRatio(window.devicePixelRatio)
     }
     handleResize()
     window.addEventListener('resize', handleResize)
@@ -20,6 +22,11 @@ export default function Home() {
       <h1 className="text-4xl font-bold">Your screensize detector</h1>
       <p className="text-2xl">Width: {width}px</p>
       <p className="text-2xl">Height: {height}px</p>
+      <p className="text-2xl">Device Ratio: {ratio}</p>
+      <p className="text-2xl">
+        Device Resolution:
+        {width * ratio} x {height * ratio}
+      </p>
     </div>
   )
 }
